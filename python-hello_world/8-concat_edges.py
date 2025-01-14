668be0af68c62f5edc196b7048163a376f8a875c
sindi16 <10029@holbertonstudents.com>
2025-01-14 13:35:51 +0000

    slice

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str1 = str[39:67] +"with Python"
+str1 = str[39:67] + "with Python"
 print(str1)
