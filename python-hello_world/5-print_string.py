aace5a57b63c45a54c7c3fd02f22119fd6a89527
sindi16 <10029@holbertonstudents.com>
2025-01-14 11:19:16 +0000

    6-concat.py

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton School"
 for i in range(0, 3):
-	print(str)
+	print(str, end = " ")
--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
-str2 = "School"
-concacenate_str = "Holberton School"
+str2 = "School!"
+concacenate_str = "Holberton School!"
 print(f"Welcome to {concacenate_str}!")
