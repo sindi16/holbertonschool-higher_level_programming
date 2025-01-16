1466f180992e083be08739d0a1e74b4f3c300576
sindi16 <10029@holbertonstudents.com>
2025-01-16 13:36:33 +0000

    fix pychode

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!usr/bin/python3
 
 for i in range(0, 9):
-    for j in range (i + 1, 10):
+    for j in range(i + 1, 10):
         number = str(i) + str(j)
         if number == "89":
             print("{}{}".format(i, j))
