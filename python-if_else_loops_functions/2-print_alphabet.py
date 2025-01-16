4a9bee25e6f336a754e4377a6d452b5d571b60ca
sindi16 <10029@holbertonstudents.com>
2025-01-16 11:57:31 +0000

    lowercase

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):
-    if i == 101 and i == 113:
-        print("{}".format(chr(i)), end="")
+for ch in range(97, 123):
+    print("{}".format(chr(ch)), end="")
--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(0, 100):
-    if i <= 9:
-        i = "0" + str(i)
-    if i != 99:
-        print("{}".format(i), end=", ")
+for number in range(100):
+    if number != 99:
+        print(f"{number:02}", end=", ")
     else:
-        print("{}".format(i))
+        print(f"{number:02}")
