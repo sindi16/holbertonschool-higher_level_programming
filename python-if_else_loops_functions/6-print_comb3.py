b917cb7fd10379f1e46eeab30a7a514275198761
sindi16 <10029@holbertonstudents.com>
2025-01-19 17:53:26 +0000

    print the numers until 89 with , and space

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,8 @@
-#!usr/bin/python3
-
-for i in range(0, 9):
-    for j in range(i + 1, 10):
-        number = str(i) + str(j)
-        if number == "89":
-            print("{}{}".format(i, j))
-        else:
-            print("{}{}".format(i, j), end=", ")
+#!/usr/bin/python3
+for i in range(10):
+    for x in range(0,10):
+        if i == 8 and x == 9:
+            print('{}{}'.format(i, x), end="\n")
+        elif i < x:
+            print('{}{}'.format(i, x), end=", ")
+        
