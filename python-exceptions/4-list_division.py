1a7ef0b2adb9170bffa1b73afd4922be46b7eeaf
sindi16 <10029@holbertonstudents.com>
2025-01-24 10:25:21 +0000

    hi

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    for i in range(list_division):
+    for i in range(0, list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
         except ZeroDivisionError:
