c9d0cba10fe998fd6d1c454e5b244eb861be3e37
sindi16 <10029@holbertonstudents.com>
2025-02-06 10:44:30 +0000

    hi

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+'''Defines a class MyList that inherits from list'''
+
+
 class MyList(list):
+    '''A class that inherits from list'''
     def print_sorted(self):
+        '''prints a sorted list'''
         print(sorted(self))
--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Module that holds a function"""
+
 def is_same_class(obj, a_class):
+    """Func to verify accordance"""
     if type(obj) is a_class:
         return True
     return False
