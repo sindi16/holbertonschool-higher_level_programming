a4acf6503f73882755af429286d1b79f39bf1a88
sindi16 <10029@holbertonstudents.com>
2025-03-05 12:27:03 +0100

    hi

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
---a script that lists all cities contained in the database hbtn_0d_usa
+--script that lists all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON states.id = cities.state_id
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
