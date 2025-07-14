fef77bbd0c964f41324ef34442db83a6173802be
sindi16 <10029@holbertonstudents.com>
2025-07-14 11:51:01 +0200

    hi

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,25 +1,23 @@
+#!/usr/bin/python3 
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# In-memory users dictionary
-users = {
-    "jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},
-    "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}
-}
+users = {}
 
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
 
 @app.route("/data")
-def data():
-    # Return a list of all usernames
+def get_data():
     return jsonify(list(users.keys()))
+
 
 @app.route("/status")
 def status():
     return "OK"
+
 
 @app.route("/users/<username>")
 def get_user(username):
@@ -31,24 +29,12 @@ def get_user(username):
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
-    data = request.get_json(force=True)
-    username = data.get("username")
-
+    user_data = request.get_json()
+    username = user_data.get("username")
     if not username:
         return jsonify({"error": "Username is required"}), 400
-
-    # Add user to dictionary
-    users[username] = {
-        "username": username,
-        "name": data.get("name"),
-        "age": data.get("age"),
-        "city": data.get("city")
-    }
-
-    return jsonify({
-        "message": "User added",
-        "user": users[username]
-    }), 201
+    users[username] = user_data
+    return jsonify({"message": "User added", "user": user_data}), 201
 
 if __name__ == "__main__":
     app.run()
