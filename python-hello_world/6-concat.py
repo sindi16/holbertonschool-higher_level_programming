dacdd79bca455d0fc9cdf7fff8ea4bfe19c304ae
sindi16 <10029@holbertonstudents.com>
2025-07-14 12:20:01 +0200

    hi

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
-str2 = "School!"
-concacenate_str = str1 + " " + str2
-print(f"Welcome to {concacenate_str}")
+str2 = "School"
+str1 += " " + str2
+print(f"Welcome to {str1}!")
